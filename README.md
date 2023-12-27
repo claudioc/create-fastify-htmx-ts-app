55c736e6e515d469cd41dfed4aa10801d44a005b
Claudio Cicali <claudio.cicali@gmail.com>
2023-12-27 16:39:13 +0100

    Adds the release task

---
+++ README.md
@@ -0,0 +1,17 @@
+## Create Fastify + HTMX + Ts Application
+
+This initializer will just download the [Fastify-HTMX-TypeScript](https://github.com/claudioc/fastify-htmx-ts-starter-kit) starter kit for you, creates an application directory and prepares a clean `package.json` for you to edit.
+
+It doesn't work on Windows machines. Feel free to fix this :)
+
+Usage:
+
+```sh
+npm create fastify-htmx-ts-app
+# or
+npx create-next-app@latest
+# or
+yarn create next-app
+# or
+pnpm create next-app
+```
--- bin/create.js
+++ bin/create.js
@@ -11,25 +11,59 @@ const main = async () => {
     process.exit(1); //an error occurred
   }
 
-  const appPath = args[0];
-  const root = path.resolve(appPath);
+  const appName = args[0];
+  const root = path.resolve(appName);
   console.log(root);
+
+  if (fs.existsSync(root)) {
+    console.error('The application path already exists.');
+    process.exit(1);
+  }
 
   if (!(await isWriteable(path.dirname(root)))) {
     console.error(
       'The application path is not writable, please check folder permissions and try again.',
     );
-    console.error('It is likely you do not have write permissions for the current folder.');
+    console.error('It is likely you do not have write permissions for this folder.');
     process.exit(1);
   }
 
   // Git clone might be safer than downloading a zip file which would require checking
   // for the presence of 'zip', 'curl' or 'wget'
-  execSync(`git clone git@github.com:claudioc/fastify-htmx-ts-starter-kit.git ${appPath}`, {
+  execSync(`git clone git@github.com:claudioc/fastify-htmx-ts-starter-kit.git ${appName}`, {
     stdio: 'ignore',
   });
 
-  console.log('created');
+  execSync('rm -rf .git', { cwd: appName, stdio: 'ignore' });
+  execSync('rm -rf tasks', { cwd: appName, stdio: 'ignore' });
+
+  const packageJsonPath = path.join(root, 'package.json');
+  const packageJson = require(packageJsonPath);
+  packageJson.name = path.basename(root);
+  packageJson.version = '0.1.0';
+  packageJson.description = '';
+  packageJson.author = '';
+  packageJson.license = 'ISC';
+  delete packageJson.repository;
+  delete packageJson.bugs;
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+  console.log(`Success! Created ${appName} at ${appName}`);
+
+  console.log(`
+We didn't install any dependencies for you; use your favourite package manager to do so (i.e. "npm install").
+
+Inside that directory, you can run several commands:
+    npm run dev
+      Starts the development server.
+
+    npm run build
+      Builds the app for production.
+
+    npm start
+      Runs the built app in production mode.
+`);
 };
 
 async function isWriteable(directory) {
